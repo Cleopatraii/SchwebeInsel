9052e4a1cda24195f074721533f46d85a8c24e10
Cleopatraii <75415374+Cleopatraii@users.noreply.github.com>
2024-06-21 18:20:05 +0200

    Update fragmentShader_island.glsl

--- shader_Programm/fragmentShader_island.glsl
+++ shader_Programm/fragmentShader_island.glsl
@@ -36,22 +36,22 @@ void main() {
    //1.Beleuchtung berechnen und Schattierun implementieren
 
    //  (1) Ambient
-   float ambi = light.ambient.r;                           // 1.环境光照 ambient
+   float ambi = light.ambient.r;                           // 1.Umgebungslicht ambient
 
    //  (2) Diffuse
    //     2.1 Erstelichtquelle
-   vec3 N = normalize(vertexZuFragmentNormal);                       // 法线方向 normal
-   vec3 L = normalize(light.position - FragPosition);    // 入射光方向 light = QuelleLicht_Position - vertex_position (view)
-   float diff = max(dot(N, L), 0.0);                     // 入射光角度 (l,n)
+   vec3 N = normalize(vertexZuFragmentNormal);                       // Normalenrichtung normal
+   vec3 L = normalize(light.position - FragPosition);    // Einfallsrichtung  light = QuelleLicht_Position - vertex_position (view)
+   float diff = max(dot(N, L), 0.0);                     
    //     2.2 Zweite Lichtquelle
-   vec3 N2 = normalize(vertexZuFragmentNormal);                       // 法线方向 normal
+   vec3 N2 = normalize(vertexZuFragmentNormal);                       // Normalenrichtung  normal
    vec3 L2 = normalize(vec3(0.0, -1.0, -8.0) - FragPosition);
-   float diff2 = max(dot(N2, L2), 0.0);                     // 入射光角度 (l,n)
+   float diff2 = max(dot(N2, L2), 0.0);                     // Einfallswinkel  (l,n)
    
    //  (3) Spekular
-   vec3 augenRichtung = vec3(0.0, 0.0, 1.0);             // 视线方向
-   vec3 reflectRichtung = normalize(reflect(-L, N));     // 反射光方向
-   float spec = pow(max(dot(augenRichtung, reflectRichtung), 0.0), mat.shininess); // 偏差夹脚 (r,a)，Phone-Beleuchtungsmodellen
+   vec3 augenRichtung = vec3(0.0, 0.0, 1.0);             // Blickrichtung
+   vec3 reflectRichtung = normalize(reflect(-L, N));     // Reflexionsrichtung
+   float spec = pow(max(dot(augenRichtung, reflectRichtung), 0.0), mat.shininess); // Abweichungswinkel (r,a)，Phone-Beleuchtungsmodellen
 
 
    // 2. Multi-Texture
@@ -59,7 +59,7 @@ void main() {
    vec4 detailColor = texture(FragTexture_stone1, texKoordinaten);
    vec4 shadowColor = texture(FragTexture_stone2, texKoordinaten);
 
-   // 3. Color berechnen (混合三种纹理)
+   // 3. Color berechnen (Mix Drei Texture)
    vec4 stoneTexture = mix(detailColor, shadowColor, shadowColor.r);  // Stone Texture
    vec3 stoneColor = mix(stoneTexture, baseColor, step(0.5, detailColor)).rgb; // Stone Basis Farbe mit Texture
    vec3 stoneMixColor = mix(vec3(0.2, 0.2, 0.2), stoneColor, ambi + diff + spec).rgb; // Lichtquelle1

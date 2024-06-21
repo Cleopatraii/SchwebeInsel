548d40be1ac8c6d8110237b4fbd085a5a3738eff
Cleopatraii <75415374+Cleopatraii@users.noreply.github.com>
2024-06-21 18:30:06 +0200

    Update fragmentShader_blur.glsl

--- shader_Programm/fragmentShader_blur.glsl
+++ shader_Programm/fragmentShader_blur.glsl
@@ -2,19 +2,19 @@
 
 #version 330 core
 
-in vec2 texKoordinaten;  // 输入的纹理坐标
-out vec4 FragColor; // 输出的片段颜色
+in vec2 texKoordinaten;  // Eingehende Texturkoordinaten
+out vec4 FragColor; //Ausgehende Fragmentfarbe
 
-uniform sampler2D image; // 输入的纹理
-uniform bool horizontal; // 控制水平或垂直模糊的布尔变量
-const float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216); // 高斯权重数组
+uniform sampler2D image; // Eingehende Textur
+uniform bool horizontal; //  Steuerung für horizontales oder vertikales Weichzeichnen
+const float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216); // Gaußsche Gewichtungen
 
 void main(){     
 
-    vec2 tex_offset = 1.0 / textureSize(image, 0);                // gets size of single texel // 获取纹理的大小
-    vec3 result = texture(image, texKoordinaten).rgb * weight[0]; // current fragment's contribution 当前像素的颜色
+    vec2 tex_offset = 1.0 / textureSize(image, 0);                // Größe eines Texels erhalten
+    vec3 result = texture(image, texKoordinaten).rgb * weight[0]; // Beitrag des aktuellen Fragments
 
-    //水平模糊
+    // Horizontale Weichzeichnung
     if(horizontal)
     {
         for(int i = 1; i < 5; ++i)
@@ -23,7 +23,7 @@ void main(){
             result += texture(image, texKoordinaten - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
         }
     }
-    //垂直模糊
+     // Vertikale Weichzeichnung
     else
     {
         for(int i = 1; i < 5; ++i)
